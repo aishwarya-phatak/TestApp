a2f61e094e6d064738abe666e6a1d9f6eedbd4a5
aishwarya <ashp17star@gmail.com>
2022-11-23 13:27:34 +0530

    Attaching Listeners To view in different ways

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -6,16 +6,22 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    TextView textView;
+    EditText edtUsername,edtPassword;
+    Button btnLogin, btnLogin1;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
 
         LinearLayout linearLayout = new LinearLayout(this);
@@ -30,7 +36,7 @@ public class MainActivity extends Activity {
 
         linearLayout.setLayoutParams(layoutParams);
 
-        TextView textView = new TextView(this);
+        textView = new TextView(this);
 
         textView.setText("Welcome To Android Batch!");
         textView.setTextColor(Color.BLACK);
@@ -46,7 +52,7 @@ public class MainActivity extends Activity {
         textView.setLayoutParams(layoutParamsForChildViews);
         linearLayout.addView(textView);
 
-        EditText edtUsername = new EditText(this);
+        edtUsername = new EditText(this);
         edtUsername.setHint("Enter username");
         edtUsername.setTextColor(Color.RED);
         edtUsername.setBackgroundColor(Color.GRAY);
@@ -55,7 +61,7 @@ public class MainActivity extends Activity {
         edtUsername.setLayoutParams(layoutParamsForChildViews);
         linearLayout.addView(edtUsername);
 
-        EditText edtPassword = new EditText(this);
+        edtPassword = new EditText(this);
         edtPassword.setHint("Enter password");
         edtPassword.setTextColor(Color.BLUE);
         edtPassword.setBackgroundColor(Color.WHITE);
@@ -63,14 +69,79 @@ public class MainActivity extends Activity {
         edtPassword.setLayoutParams(layoutParamsForChildViews);
         linearLayout.addView(edtPassword);
 
-        Button btnSubmit = new Button(this);
-        btnSubmit.setText("Login");
-        btnSubmit.setTextColor(Color.WHITE);
-        btnSubmit.setBackgroundColor(Color.BLACK);
+        btnLogin = new Button(this);
+        btnLogin.setText("Login");
+        btnLogin.setTextColor(Color.WHITE);
+        btnLogin.setBackgroundColor(Color.BLACK);
+        btnLogin.setTextSize(16);
 
-        btnSubmit.setLayoutParams(layoutParamsForChildViews);
-        linearLayout.addView(btnSubmit);
+        btnLogin.setLayoutParams(layoutParamsForChildViews);
+        linearLayout.addView(btnLogin);
 
         setContentView(linearLayout);
+
+        /*way 2
+        View.OnClickListener listener = new MyOnClickListener();
+        btnLogin.setOnClickListener(listener);
+        textView.setOnClickListener(listener);
+         */
+
+
+        /*way 3 - implementing at class level
+        textView.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+         */
+
+        //way 1
+        textView.setOnClickListener(new TextViewClickListener());
+        btnLogin.setOnClickListener(new BtnLoginClickListener());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == textView){
+            textView.setText("");
+        }
+        if(view == btnLogin){
+            if(edtUsername.getText().toString().equals("bitcode")&& edtPassword.getText().toString().equals("bitcode123")){
+                textView.setText("Welcome "+edtUsername.getText().toString());
+            }
+        }
+    }
+
+    class MyOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            if(view == textView){
+                textView.setText("");
+            }
+            if(view == btnLogin){
+                if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode123")){
+                    textView.setText("Welcome "+edtUsername.getText().toString());
+                } else {
+                    textView.setText("");
+                }
+            }
+        }
+    }
+
+    class TextViewClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            textView.setText("");
+        }
+    }
+
+    class BtnLoginClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode123")){
+                textView.setText("Welcome " + edtUsername.getText().toString());
+            } else {
+                textView.setText("Login Failed!");
+            }
+        }
     }
 }
